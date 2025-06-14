3efe18a417218ed3fde78e3314ac959a8f2b410c
Vivek <vivek.bisht555@gmail.com>
2025-06-15 03:03:12 +0530

    adding services

---
+++ doctor-service/src/test/java/com/vivek/firstmed/doctor_service/DoctorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vivek.firstmed.doctor_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
