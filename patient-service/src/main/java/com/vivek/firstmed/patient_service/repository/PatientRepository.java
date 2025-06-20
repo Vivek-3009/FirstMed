751517c4a93a8c36929c78abf6e71514af8f6e41
Vivek <vivek.bisht555@gmail.com>
2025-06-21 02:26:14 +0530

    Added file to patient-service

---
+++ patient-service/src/main/java/com/vivek/firstmed/patient_service/repository/PatientRepository.java
@@ -0,0 +1,9 @@
+package com.vivek.firstmed.patient_service.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.vivek.firstmed.patient_service.entity.Patient;
+
+public interface PatientRepository extends JpaRepository<Patient,String>{
+    
+}
