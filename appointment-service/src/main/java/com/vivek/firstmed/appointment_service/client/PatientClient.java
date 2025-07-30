ac1959480103cac566af39dff736b5d439b3830f
Vivek <vivek.bisht555@gmail.com>
2025-07-31 02:30:22 +0530

    Patient Client in Appointment service

---
+++ appointment-service/src/main/java/com/vivek/firstmed/appointment_service/client/PatientClient.java
@@ -0,0 +1,11 @@
+package com.vivek.firstmed.appointment_service.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "patient-service", url = "${patient.service.url}")
+public interface PatientClient {
+    @GetMapping("/internal/patient/{patientId}")
+    PatientDto getPatientById(@PathVariable String patientId);
+}
