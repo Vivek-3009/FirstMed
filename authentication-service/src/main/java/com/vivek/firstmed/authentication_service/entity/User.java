61be84d49fae7ee3ba1d18df8ede2e5c95255c3d
Vivek <vivek.bisht555@gmail.com>
2025-08-24 20:42:13 +0530

    Added User in Auth-service

---
+++ authentication-service/src/main/java/com/vivek/firstmed/authentication_service/entity/User.java
@@ -0,0 +1,62 @@
+package com.vivek.firstmed.authentication_service.entity;
+
+import java.util.Set;
+
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+import org.hibernate.annotations.Where;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@SQLDelete(sql = "UPDATE users SET is_deleted = true WHERE user_id = ?")
+@SQLRestriction("is_deleted = false")
+public class User {
+
+    @Id
+    @Column(name = "user_id", nullable = false, updatable = false)
+    private String userId; // You can use your central idGeneratorService
+
+    @Column(unique = true, nullable = false)
+    private String username; // Can be email/phone
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(unique = true)
+    private String email;
+
+    private boolean enabled = true;
+
+    private boolean accountNonLocked = true;
+
+    @Builder.Default
+    private boolean isDeleted = false;
+
+    // Many-to-Many with roles
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+        name = "user_roles",
+        joinColumns = @JoinColumn(name = "user_id"),
+        inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles;
+}
