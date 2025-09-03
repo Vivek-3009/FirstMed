1f7b13dee6a94e349d57265926a6fe0b37e555fd
Vivek <vivek.bisht555@gmail.com>
2025-09-03 21:20:17 +0530

    modified AppUserDetails.java to authentication_service

--- authentication-service/src/main/java/com/vivek/firstmed/authentication_service/security/AppUserDetails.java
+++ authentication-service/src/main/java/com/vivek/firstmed/authentication_service/security/AppUserDetails.java
@@ -23,13 +23,13 @@ public class AppUserDetails implements UserDetails {
                 .map(r -> new SimpleGrantedAuthority(r.getRoleName()))
                 .collect(Collectors.toSet());
     }
-    // @Override public String getPassword() { return user.getPassword(); }
-    // @Override public String getUsername() { return user.getUsername(); }
-    // @Override public boolean isAccountNonExpired() { return true; }
-    // @Override public boolean isAccountNonLocked() { return user.isAccountNonLocked(); }
-    // @Override public boolean isCredentialsNonExpired() { return true; }
-    // @Override public boolean isEnabled() { return user.isEnabled(); }
+    @Override public String getPassword() { return user.getPassword(); }
+    @Override public String getUsername() { return user.getUsername(); }
+    @Override public boolean isAccountNonExpired() { return true; }
+    @Override public boolean isAccountNonLocked() { return user.isAccountNonLocked(); }
+    @Override public boolean isCredentialsNonExpired() { return true; }
+    @Override public boolean isEnabled() { return user.isEnabled(); }
 
-    // public String getUserId() { return user.getUserId(); }
+    public String getUserId() { return user.getUserId(); }
 }
     
