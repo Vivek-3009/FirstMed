f03f68105cefcf5acdd9a75d2bba4526bf8f298a
Vivek <vivek.bisht555@gmail.com>
2025-09-06 03:39:34 +0530

    Modified UserRepository.java to authentication_service

--- authentication-service/src/main/java/com/vivek/firstmed/authentication_service/repository/UserRepository.java
+++ authentication-service/src/main/java/com/vivek/firstmed/authentication_service/repository/UserRepository.java
@@ -7,4 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.vivek.firstmed.authentication_service.entity.User;
 
 public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByUsername(String username);
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
 }
