25ce1ae7142fafc56d49ba502df608adc6de559b
Vivek <vivek.bisht555@gmail.com>
2025-09-07 22:52:12 +0530

    Modified RoleRepository.java to authentication_service

--- authentication-service/src/main/java/com/vivek/firstmed/authentication_service/repository/RoleRepository.java
+++ authentication-service/src/main/java/com/vivek/firstmed/authentication_service/repository/RoleRepository.java
@@ -1,9 +1,11 @@
 package com.vivek.firstmed.authentication_service.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.vivek.firstmed.authentication_service.entity.Role;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
-    
+    Optional<Role> findByRoleName(String roleName);
 }
