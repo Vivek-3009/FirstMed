3e6ad4ecf42fdf70df1161009cca29f9b0a9d8b5
Vivek <vivek.bisht555@gmail.com>
2025-09-11 00:00:48 +0530

    Modified RefreshTokenRepository .java to authentication_service

--- authentication-service/src/main/java/com/vivek/firstmed/authentication_service/repository/RefreshTokenRepository.java
+++ authentication-service/src/main/java/com/vivek/firstmed/authentication_service/repository/RefreshTokenRepository.java
@@ -1,9 +1,12 @@
 package com.vivek.firstmed.authentication_service.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.vivek.firstmed.authentication_service.entity.RefreshToken;
 
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
-    
+    Optional<RefreshToken> findByToken(String token);
+    void deleteByToken(String token);
 }
