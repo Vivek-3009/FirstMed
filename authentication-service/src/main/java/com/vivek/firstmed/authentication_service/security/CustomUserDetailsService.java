9339df34e76ad1410d6e8d74459ae138e2441930
Vivek <vivek.bisht555@gmail.com>
2025-09-13 00:50:23 +0530

    Modified CustomeUserDetailsService.java to authentication_service

--- authentication-service/src/main/java/com/vivek/firstmed/authentication_service/security/CustomUserDetailsService.java
+++ authentication-service/src/main/java/com/vivek/firstmed/authentication_service/security/CustomUserDetailsService.java
@@ -1,16 +1,26 @@
 package com.vivek.firstmed.authentication_service.security;
 
 
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import com.vivek.firstmed.authentication_service.entity.User;
+import com.vivek.firstmed.authentication_service.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
+    
+    private final UserRepository userRepository;
 
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User u = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
+        return new AppUserDetails(u);
+    }
 }
