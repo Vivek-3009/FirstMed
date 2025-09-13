28d2ac6ed82fc9e0529cdc5182546d120934091b
Vivek <vivek.bisht555@gmail.com>
2025-09-14 00:09:26 +0530

    Modified AppUserDetails.java to authentication_service

--- authentication-service/src/main/java/com/vivek/firstmed/authentication_service/security/AppUserDetails.java
+++ authentication-service/src/main/java/com/vivek/firstmed/authentication_service/security/AppUserDetails.java
@@ -9,13 +9,14 @@ import java.util.Set;
 import java.util.stream.Collectors;
 import com.vivek.firstmed.authentication_service.entity.User;
 
-
 @Getter
 public class AppUserDetails implements UserDetails {
 
     private final User user;
 
-    public AppUserDetails(User user) { this.user = user; }
+    public AppUserDetails(User user) {
+        this.user = user;
+    }
 
     @Override
     public Set<GrantedAuthority> getAuthorities() {
@@ -23,13 +24,38 @@ public class AppUserDetails implements UserDetails {
                 .map(r -> new SimpleGrantedAuthority(r.getRoleName()))
                 .collect(Collectors.toSet());
     }
-    @Override public String getPassword() { return user.getPassword(); }
-    @Override public String getUsername() { return user.getUsername(); }
-    @Override public boolean isAccountNonExpired() { return true; }
-    @Override public boolean isAccountNonLocked() { return user.isAccountNonLocked(); }
-    @Override public boolean isCredentialsNonExpired() { return true; }
-    @Override public boolean isEnabled() { return user.isEnabled(); }
 
-    public String getUserId() { return user.getUserId(); }
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return user.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return user.isEnabled();
+    }
+
+    public String getUserId() {
+        return user.getUserId();
+    }
 }
-    
