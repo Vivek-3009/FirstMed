d9c10ab8605b10d12ed1cddb5aa0ea7bc2a94891
Vivek <vivek.bisht555@gmail.com>
2025-09-17 01:04:55 +0530

    Modified JwtUtil.java inauthentication_service

--- authentication-service/src/main/java/com/vivek/firstmed/authentication_service/jwt/JwtUtil.java
+++ authentication-service/src/main/java/com/vivek/firstmed/authentication_service/jwt/JwtUtil.java
@@ -1,8 +1,11 @@
 package com.vivek.firstmed.authentication_service.jwt;
 
+import javax.crypto.SecretKey;
+
 import org.springframework.stereotype.Component;
 
 @Component
 public class JwtUtil {
+    private final SecretKey key;
     
 }
