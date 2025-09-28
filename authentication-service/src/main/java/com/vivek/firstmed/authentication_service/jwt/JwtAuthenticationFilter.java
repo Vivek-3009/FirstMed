8f2a113894fca69772f1559f39670d41821f7cf2
Vivek <vivek.bisht555@gmail.com>
2025-09-29 00:33:39 +0530

    Modified JwtAuthenticationFilter .java in authentication_service

--- authentication-service/src/main/java/com/vivek/firstmed/authentication_service/jwt/JwtAuthenticationFilter.java
+++ authentication-service/src/main/java/com/vivek/firstmed/authentication_service/jwt/JwtAuthenticationFilter.java
@@ -4,12 +4,17 @@ import java.io.IOException;
 
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import com.vivek.firstmed.authentication_service.security.CustomUserDetailsService;
+
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
+ 
 public class JwtAuthenticationFilter extends OncePerRequestFilter{
+
+    private final JwtUtil jwtUtil;
+    private final CustomUserDetailsService userDetailsService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
