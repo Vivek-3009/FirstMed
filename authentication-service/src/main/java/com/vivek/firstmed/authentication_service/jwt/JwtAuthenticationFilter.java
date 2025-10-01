7ddeaf98836b7ccb63ee68fe74905e5f0dec3d15
Vivek <vivek.bisht555@gmail.com>
2025-10-01 23:50:45 +0530

    Modified JwtAuthenticationFilter .java in authentication_service

--- authentication-service/src/main/java/com/vivek/firstmed/authentication_service/jwt/JwtAuthenticationFilter.java
+++ authentication-service/src/main/java/com/vivek/firstmed/authentication_service/jwt/JwtAuthenticationFilter.java
@@ -18,6 +18,11 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter{
 
     private final JwtUtil jwtUtil;
     private final CustomUserDetailsService userDetailsService;
+    
+    public JwtAuthenticationFilter(JwtUtil jwtUtil, CustomUserDetailsService userDetailsService) {
+    this.jwtUtil = jwtUtil;
+    this.userDetailsService = userDetailsService;
+}
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
