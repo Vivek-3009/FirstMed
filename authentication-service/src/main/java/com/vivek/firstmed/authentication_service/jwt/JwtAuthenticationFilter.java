d1705afb3820f6dc5d72c7cc1646fd1fd308e392
Vivek <vivek.bisht555@gmail.com>
2025-10-04 00:52:56 +0530

    Modified JwtAuthenticationFilter .java in authentication_service

--- authentication-service/src/main/java/com/vivek/firstmed/authentication_service/jwt/JwtAuthenticationFilter.java
+++ authentication-service/src/main/java/com/vivek/firstmed/authentication_service/jwt/JwtAuthenticationFilter.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import com.vivek.firstmed.authentication_service.security.CustomUserDetailsService;
@@ -13,16 +14,14 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
  
+@Component
+@RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter{
 
     private final JwtUtil jwtUtil;
     private final CustomUserDetailsService userDetailsService;
-
-    public JwtAuthenticationFilter(JwtUtil jwtUtil, CustomUserDetailsService userDetailsService) {
-    this.jwtUtil = jwtUtil;
-    this.userDetailsService = userDetailsService;
-    }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request,
