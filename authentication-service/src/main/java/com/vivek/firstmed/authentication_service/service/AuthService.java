8b516ae1505a966c29f2bb069be74f8e84124d3d
Vivek <vivek.bisht555@gmail.com>
2025-10-05 01:03:29 +0530

    Added AuthService.java in authentication_service

---
+++ authentication-service/src/main/java/com/vivek/firstmed/authentication_service/service/AuthService.java
@@ -0,0 +1,11 @@
+package com.vivek.firstmed.authentication_service.service;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+    
+}
