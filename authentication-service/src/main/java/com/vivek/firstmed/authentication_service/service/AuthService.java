bed6cd7637293fa02f56220b90a3301c64e39ca0
Vivek <vivek.bisht555@gmail.com>
2025-10-06 00:59:56 +0530

    Modified AuthService.java in authentication_service

--- authentication-service/src/main/java/com/vivek/firstmed/authentication_service/service/AuthService.java
+++ authentication-service/src/main/java/com/vivek/firstmed/authentication_service/service/AuthService.java
@@ -1,11 +1,24 @@
 package com.vivek.firstmed.authentication_service.service;
 
+import org.springframework.core.env.Environment;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import com.vivek.firstmed.authentication_service.jwt.JwtUtil;
+import com.vivek.firstmed.authentication_service.repository.RefreshTokenRepository;
+import com.vivek.firstmed.authentication_service.repository.RoleRepository;
+import com.vivek.firstmed.authentication_service.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class AuthService {
+    private final UserRepository userRepo;
+    private final RoleRepository roleRepo;
+    private final RefreshTokenRepository refreshRepo;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
+    private final Environment env;
     
 }
