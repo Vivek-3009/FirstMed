28c4f6f203f70263d9919aac4f2312d9ba36e027
Vivek <vivek.bisht555@gmail.com>
2025-10-11 00:49:23 +0530

    Modified AuthService.java in authentication_service

--- authentication-service/src/main/java/com/vivek/firstmed/authentication_service/service/AuthService.java
+++ authentication-service/src/main/java/com/vivek/firstmed/authentication_service/service/AuthService.java
@@ -1,5 +1,7 @@
 package com.vivek.firstmed.authentication_service.service;
 
+import java.util.Set;
+
 import org.springframework.core.env.Environment;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -7,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.vivek.firstmed.authentication_service.dto.RegisterRequest;
 import com.vivek.firstmed.authentication_service.entity.Role;
+import com.vivek.firstmed.authentication_service.entity.User;
 import com.vivek.firstmed.authentication_service.jwt.JwtUtil;
 import com.vivek.firstmed.authentication_service.repository.RefreshTokenRepository;
 import com.vivek.firstmed.authentication_service.repository.RoleRepository;
@@ -35,6 +38,15 @@ public class AuthService {
         Role role = roleRepo.findByRoleName("ROLE_PATIENT")
                 .orElseGet(() -> roleRepo.save(Role.builder().roleName("ROLE_PATIENT").build()));
 
+        User user = User.builder()
+                .userId("U" + System.currentTimeMillis())
+                .username(req.getUsername())
+                .email(req.getEmail())
+                .password(passwordEncoder.encode(req.getPassword()))
+                .enabled(true)
+                .accountNonLocked(true)
+                .roles(Set.of(role))
+                .build();
     }
     
 }
