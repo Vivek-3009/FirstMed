0ba7720e21348488fd4b345a229749dee6448e7b
Vivek <vivek.bisht555@gmail.com>
2025-11-16 00:21:52 +0530

    Modified SecurityConfig.java in authentication_service

--- authentication-service/src/main/java/com/vivek/firstmed/authentication_service/config/SecurityConfig .java
+++ authentication-service/src/main/java/com/vivek/firstmed/authentication_service/config/SecurityConfig .java
@@ -3,6 +3,7 @@ package com.vivek.firstmed.authentication_service.config;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig  {
     
 }
