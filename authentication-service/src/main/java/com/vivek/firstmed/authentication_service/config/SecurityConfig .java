75d8a54a50344cb430550973e839d5068926fde0
Vivek <vivek.bisht555@gmail.com>
2025-11-18 23:33:32 +0530

    Modified SecurityConfig.java in authentication_service

--- authentication-service/src/main/java/com/vivek/firstmed/authentication_service/config/SecurityConfig .java
+++ authentication-service/src/main/java/com/vivek/firstmed/authentication_service/config/SecurityConfig .java
@@ -2,10 +2,14 @@ package com.vivek.firstmed.authentication_service.config;
 
 import org.springframework.context.annotation.Configuration;
 
+import com.vivek.firstmed.authentication_service.jwt.JwtAuthenticationFilter;
+
 import lombok.RequiredArgsConstructor;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig  {
+
+    private final JwtAuthenticationFilter jwtFilter;
     
 }
