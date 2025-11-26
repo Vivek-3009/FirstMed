c885548036d2628707226dd174f57fe4570949d5
Vivek <vivek.bisht555@gmail.com>
2025-11-27 01:51:22 +0530

    Modified SecurityConfig.java in authentication_service

--- authentication-service/src/main/java/com/vivek/firstmed/authentication_service/config/SecurityConfig.java
+++ authentication-service/src/main/java/com/vivek/firstmed/authentication_service/config/SecurityConfig.java
@@ -1,8 +1,12 @@
 package com.vivek.firstmed.authentication_service.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.vivek.firstmed.authentication_service.jwt.JwtAuthenticationFilter;
 
@@ -26,6 +30,7 @@ public class SecurityConfig  {
     }
 
     public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
     
 }
