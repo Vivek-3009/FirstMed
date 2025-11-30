9c1655269dc1b58ea0d371359dca9faba105d2d6
Vivek <vivek.bisht555@gmail.com>
2025-12-01 01:21:34 +0530

    Modified SecurityConfig.java in authentication_service

--- authentication-service/src/main/java/com/vivek/firstmed/authentication_service/config/SecurityConfig.java
+++ authentication-service/src/main/java/com/vivek/firstmed/authentication_service/config/SecurityConfig.java
@@ -38,6 +38,7 @@ public class SecurityConfig  {
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration cfg) throws Exception {
+        return cfg.getAuthenticationManager();
     }
     
 }
